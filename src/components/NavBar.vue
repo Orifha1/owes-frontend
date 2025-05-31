26055d3bcc02f6e2d0db3f89a10ae08974d7cdfe
Orifha <orifhatshikotshi1@gmail.com>
2025-05-31 17:17:43 +0200

    add form

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,6 +4,11 @@
 <template>
     <header class="header">
         <h1>navbar</h1>
+        <img src="" alt="logo">
+        <form action="" class="search">
+            <input type="text" class="search__input" placeholder="Search transactions">
+            <button class="search__button"></button>
+        </form>
     </header>
 </template>
 
